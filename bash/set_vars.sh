1ab813d619bf85a8c4c1206b6557359105f0facd
deus <deus3.deus6.deus9@gmail.com>
2023-01-27 01:42:53 +1100

    Improved run script using bash and seperate process.

--- bash/set_vars.sh
+++ bash/set_vars.sh
@@ -5,6 +5,8 @@ project_directory=$(pwd)
 script_location=$project_directory/bash/run_latest.sh # $HOME/zoxel/documents/zoxel_web/run_latest.sh
 install_location=$HOME/.profile #.bash_profile
 # run_command="x-terminal-emulator -f -e '$script_location'"
-run_command="gnome-terminal -- $script_location"
+# run_command="gnome-terminal -- $script_location"
+# run_command="x-terminal-emulator -H -e sleep 12; gnome-terminal -- $script_location"
+run_command="bash -c 'sleep 6; gnome-terminal --full-screen -q -- $script_location' &"
 
 echo "Location of script is: $script_location"
