46ae47250e25f6777714b5423eebff9a38cb8006
deus <deus3.deus6.deus9@gmail.com>
2023-01-27 01:12:05 +1100

    Updated run latest script.

--- bash/set_vars.sh
+++ bash/set_vars.sh
@@ -4,6 +4,7 @@ cd .. # go to project folder
 project_directory=$(pwd)
 script_location=$project_directory/bash/run_latest.sh # $HOME/zoxel/documents/zoxel_web/run_latest.sh
 install_location=$HOME/.profile #.bash_profile
-run_command="x-terminal-emulator -f -e '$script_location'"
+# run_command="x-terminal-emulator -f -e '$script_location'"
+run_command="gnome-terminal -- $script_location"
 
 echo "Location of script is: $script_location"
