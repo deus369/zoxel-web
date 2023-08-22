6d88f66db6e587301dd69b675a1ac8ff72533ad0
deus <deus3.deus6.deus9@gmail.com>
2023-08-22 14:10:53 +0800

    reimplemented autostart with desktop file

--- bash/install_startup.sh
+++ bash/install_startup.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 source bash/set_vars.sh
-
 echo "  > installing startup bash command"
 
-# Check if the $install_location file exists
-if [ ! -f $install_location ]
-then
-    # $install_location file does not exist, so create it
-    touch $install_location
-    echo "  + created file [$install_location] "
-else
-    echo "  > file already exists [$install_location]"
-fi
+# # Check if the $install_location file exists
+# if [ ! -f $install_location ]
+# then
+#     # $install_location file does not exist, so create it
+#     touch $install_location
+#     echo "  + created file [$install_location] "
+# else
+#     echo "  > file already exists [$install_location]"
+# fi
 
-# Check if the command has already been added to $install_location
-if [[ $(grep "$run_command" $install_location) == $run_command ]]
-then
-    # Command already added
-    echo "  > command found in [$install_location]"
-else
-    # Command not added, so add it
-    echo $run_command >> $install_location
-    echo "  + command added to [$install_location]"
-fi
+# # Check if the command has already been added to $install_location
+# if [[ $(grep "$run_command" $install_location) == $run_command ]]
+# then
+#     # Command already added
+#     echo "  > command found in [$install_location]"
+# else
+#     # Command not added, so add it
+#     echo $run_command >> $install_location
+#     echo "  + command added to [$install_location]"
+# fi
+
+echo "  > copying [bash/$desktop_filename] to [$desktop_install_path/$desktop_filename]"
+
+cp "bash/$desktop_filename" "$desktop_install_path/$desktop_filename"
+
+echo "  > copying [bash/$desktop_filename] to [$HOME/.config/autostart/$desktop_filename]"
+cp "bash/$desktop_filename" "$HOME/.config/autostart/$desktop_filename"
--- bash/uninstall_startup.sh
+++ bash/uninstall_startup.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 source bash/set_vars.sh
-
 echo "  > uninstalling startup bash command"
 
-if [ ! -f $install_location ]
-then
-    echo "  > [$install_location] file does not exist"
-    sleep 3
-    exit
-fi
+# if [ ! -f $install_location ]
+# then
+#     echo "  > [$install_location] file does not exist"
+#     sleep 3
+#     exit
+# fi
 
-# Check if the command is present in .bash_profile
-if [[ $(grep "$run_command" $install_location) == $run_command ]]
-then
-    # Command is present, so remove it
-    converted_string=$(echo "$run_command" | sed -e 's/[$]/\\$/g' -e 's/[\/]/\\\//g')
-    sed -i "/$converted_string/d" $install_location
-    echo "  - command removed from [$install_location]"
-    echo "      - command converted_string was [$converted_string]"
-else
-    # Command is not present
-    echo "  > command not found in [$install_location]"
-fi
+# # Check if the command is present in .bash_profile
+# if [[ $(grep "$run_command" $install_location) == $run_command ]]
+# then
+#     # Command is present, so remove it
+#     converted_string=$(echo "$run_command" | sed -e 's/[$]/\\$/g' -e 's/[\/]/\\\//g')
+#     sed -i "/$converted_string/d" $install_location
+#     echo "  - command removed from [$install_location]"
+#     echo "      - command converted_string was [$converted_string]"
+# else
+#     # Command is not present
+#     echo "  > command not found in [$install_location]"
+# fi
+
+echo "  > removing [$desktop_install_path/$desktop_filename]"
+rm $desktop_install_path/$desktop_filename
+
+echo "  > removing[$HOME/.config/autostart/$desktop_filename]"
+rm "$HOME/.config/autostart/$desktop_filename"
