9701da13ac96920b39560ccb5ccefbecbe897c68
deus <deus3.deus6.deus9@gmail.com>
2024-08-02 00:04:41 +1000

    added windows build to zoxel-web so users can download it directly here!

--- src/zoxel_sockets.c
+++ src/zoxel_sockets.c
@@ -126,10 +126,108 @@ void close_client_socket(
 #endif
 }
 
+void send_client_message(int sock, const char *buffer) {
+#ifdef _WIN32
+    send(sock, buffer, (int) strlen(buffer), 0);
+#else
+    write(sock, buffer, strlen(buffer));
+#endif
+}
+
+int send_redirect(int client_sock, const char *url) {
+    // Send HTTP redirect headers
+    char http_response[256];
+    snprintf(http_response, sizeof(http_response),
+             "HTTP/1.1 303 See Other\r\n"
+             "Location: %s\r\n"
+             "Content-Type: text/html\r\n"
+             "\r\n"
+             "<html><body><h1>File Downloaded. Redirecting...</h1></body></html>", url);
+    send(client_sock, http_response, strlen(http_response), 0);
+    return 0;   // no error
+}
+
+int send_file(int client_sock, const char *file_path, const char *filename) {
+    FILE *file;
+    char buffer[1024];
+    size_t file_size, read_size;
+    // Open the file to be served
+    file = fopen(file_path, "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        close_client_socket(client_sock);
+        return 1;
+    }
+    // Get file size
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    // Send HTTP headers
+    char http_response[256];
+    snprintf(http_response, sizeof(http_response),
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: application/octet-stream\r\n"
+             "Content-Disposition: attachment; filename=\"%s\"\r\n"
+             "Content-Length: %zu\r\n"
+             "\r\n", filename, file_size);
+    send(client_sock, http_response, strlen(http_response), 0);
+    // Send file content
+    while ((read_size = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        send(client_sock, buffer, read_size, 0);
+    }
+    // Close the file
+    fclose(file);
+    return 0;   // no error
+}
+
+int send_html_response(int client_sock, const char *html_content) {
+    char http_response[1024];
+    snprintf(http_response, sizeof(http_response),
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/html\r\n"
+             "Content-Length: %zu\r\n"
+             "\r\n%s", strlen(html_content), html_content);
+    send(client_sock, http_response, strlen(http_response), 0);
+    return 0;   // no error
+}
+
+int respond_to_client(int client_sock, char *client_message, int read_size) {
+    client_message[read_size] = '\0';
+    zox_log(" > Sending response to client");
+    // Parse the HTTP request line to extract the URL
+    char method[16], url[256], protocol[16];
+    if (sscanf(client_message, "%s %s %s", method, url, protocol) == 3) {
+        zox_log(" - Method: %s", method);
+        zox_log(" - URL: %s", url);
+        zox_log(" - Protocol: %s", protocol);
+        // Check if the URL is "/extensions"
+        // if file URL: /favicon.ico -> send back file
+        if (strcmp(url, "/") == 0) {
+            send_client_message(client_sock, response);
+        } else if (strcmp(url, "/zoxel_windows_x64.zip") == 0) {
+            send_file(client_sock, "builds/zoxel_windows_x64.zip", "zoxel_windows_x64.zip");
+        } else if (strcmp(url, "/play") == 0) {
+            // Send a simple HTTP response
+            const char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\ntodo: live zoxel here\n";
+            send_client_message(client_sock, response);
+        } else {
+            // for 404 we just send them back to main website
+            // Send a 404 Not Found response
+            // const char *response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nPage not found\n";
+            // send_client_message(client_sock, response);
+            send_client_message(client_sock, response);
+        }
+    } else {
+        // send_client_message(client_sock, response);
+        // Send a 400 Bad Request response
+        const char *response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid request\n";
+        send_client_message(client_sock, response);
+    }
+}
+
 // debug check if this stalls
 int accept_incoming_connections(int web_sock, char *response) {
     struct sockaddr_in client;
-    char client_message[2000];
     int c = sizeof(struct sockaddr_in);
 
     // Accept connection from an incoming client
@@ -164,32 +262,24 @@ int accept_incoming_connections(int web_sock, char *response) {
         }
     }
 
-    char *client_ip = inet_ntoa(client.sin_addr);
-    zox_log(" > Connection accepted from [%s]", client_ip);
     num_clients++;
+    char *client_ip = inet_ntoa(client.sin_addr);
+    zox_log(" > Connection accepted from [%s] num [%i]", client_ip, num_clients);
     is_dirty = 1;
 
     // Receive a message from client
-#ifdef _WIN32
+    char client_message[2000];
     int read_size = recv(client_sock, client_message, sizeof(client_message) - 1, 0);
-#else
-    int read_size = recv(client_sock, client_message, sizeof(client_message) - 1, 0);
-#endif
-    zox_log(" > Received %d bytes from client", read_size);
+    zox_log(" > Received %d bytes from client [%i]", read_size, client_sock);
     if (read_size >= 0) {
-        zox_log(" > Sending response to client");
-#ifdef _WIN32
-        send(client_sock, response, (int) strlen(response), 0);
-#else
-        write(client_sock, response, strlen(response));
-#endif
+        respond_to_client(client_sock, client_message, read_size);
     } else if (read_size == -1) {
-        if (!is_terminal_ui) perror("recv failed");
+        if (!is_terminal_ui) perror("   ! client message failed");
+        // send this back anyway?
+        // send_client_message(client_sock, response);
     }
-
     // Close the client socket
     close_client_socket(client_sock);
-
     return 0;   // no error
 }
 
